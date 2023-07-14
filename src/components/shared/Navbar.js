66cffc8819b4fde7d0221e2556394b70b64034ad
ArefinShaon <arefinshaon99@gmail.com>
2023-07-14 18:12:40 +0600

    fixed error

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -5,8 +5,8 @@ import boy from '../../assets/boyimg.png';
 const Navbar = () => {
 
   return (
-    <div className="h-28 bg-blackdeep " >
-      <div className="uppercase navbar  fixed z-20 text-grayLight sm:text-black font-sans font-sm" >
+    <div className="h-28  bg-blackdeep " >
+      <div className="uppercase navbar  bg-blackdeep   fixed z-20 text-grayLight sm:text-black font-sans font-sm" >
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
